bbd7727b114e8414a970f39fc6a66f4ac295d08b
Corgi <hwanggumedia@gmail.com>
2021-02-25 13:48:48 +0900

    feat: Add progress #107 'Chat Application'

--- src/resolvers/Subscription.js
+++ src/resolvers/Subscription.js
@@ -9,6 +9,31 @@ const newChat = {
   },
 }
 
+function newUserSubscribe(parent, args, context) {
+  return context.pubsub.asyncIterator("NEW_USER");
+}
+
+const newUser = {
+  subscribe: newUserSubscribe,
+  resolve: payload => {
+    return payload;
+  },
+}
+
+function removeUserSubscribe(parent, args, context) {
+  return context.pubsub.asyncIterator("REMOVE_USER");
+}
+
+const removeUser = {
+  subscribe: removeUserSubscribe,
+  resolve: payload => {
+    return payload;
+  },
+}
+
+
 module.exports = {
   newChat,
+  newUser,
+  removeUser,
 }
